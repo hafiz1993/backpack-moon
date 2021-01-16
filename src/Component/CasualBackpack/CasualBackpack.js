92e68909956f95df9ae55e5bf6b156fffabdafb5
hafiz1993 <hafizurrahaman04@gmail.com>
2021-01-16 18:41:07 +0600

    3rd

--- src/Component/CasualBackpack/CasualBackpack.js
+++ src/Component/CasualBackpack/CasualBackpack.js
@@ -3,33 +3,33 @@ import img4 from '../../images/pngwing 1.png';
 
 const CasualBackpack = () => {
     return (
-        <div class="container">
-            <div>
+        <div class="pl-5">
+            <div className="col-md-6">
                 <h2 style={{textAlign:'left', color:'#004959'}}>Casual BackPack</h2>
             </div>
-            <div className="row mt-5  d-flex justify-content-center ">
-            <div class="card text-justify mr-4 mt-3" style={{width: '18rem'}}>
-  <img src={img4} class="card-img-top" alt="..."/>
+            <div className=" row  d-flex justify-content-center mb-5">
+            <div class="card text-justify mr-4 mt-3" style={{width: '20rem'}}>
+  <img  src={img4} class="card-img-top p-3" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">Nike Sportswear Hayward</h5>
+    <p>Lorem Ipsum is simply dummy text of theprinting and typesetting industry. </p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 
-<div class="card text-justify mr-4 mt-3" style={{width: '18rem'}}>
-  <img src={img4} class="card-img-top" alt="..."/>
+<div class="card text-justify mr-4 mt-3" style={{width: '20rem'}}>
+  <img src={img4} class="card-img-top p-3" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">School backpack</h5>
+    <p>Lorem Ipsum is simply dummy text of theprinting and typesetting industry. </p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-<div class="card text-justify mr-4 mt-3" style={{width: '18rem'}}>
-  <img src={img4} class="card-img-top" alt="..."/>
+<div class="card text-justify mr-4 mt-3" style={{width: '20rem'}}>
+  <img src={img4} class="card-img-top p-3" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">CityLite Security Backpack</h5>
+    <p>Lorem Ipsum is simply dummy text of theprinting and typesetting industry. </p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>            
